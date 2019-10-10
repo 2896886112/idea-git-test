83076bc733db6a574ece428cac77b999826e34c3
houlai <2896886112@qq.com>
2019-10-10 16:55:32 +0800

    添加Roled类

---
+++ src/entity/Roled.java
@@ -0,0 +1,13 @@
+package entity;
+
+public class Roled {
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
